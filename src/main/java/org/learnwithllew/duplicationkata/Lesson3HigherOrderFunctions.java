139a3418159f821527895aaa48f932a01c7dd08c
marve <silentmarve@web.de>
2023-06-05 19:21:48 +0200

    Used IntUnaryOperatorˆ

--- src/main/java/org/learnwithllew/duplicationkata/Lesson3HigherOrderFunctions.java
+++ src/main/java/org/learnwithllew/duplicationkata/Lesson3HigherOrderFunctions.java
@@ -1,41 +1,37 @@
 package org.learnwithllew.duplicationkata;
 
-public class Lesson3HigherOrderFunctions extends Song
-{
-  public void singCheers()
-  {
-    int number = 2;
-    sing(number + "! ");
-    number = number + 2;
-    sing(number + "! ");
-    number = number + 2;
-    sing(number + "! ");
-    number = number + 2;
-    sing(number + "! ");
-    sing("Who do we appreciate?");
-    number = 17;
-    sing(number + "! ");
-    number = getNextPrime(number);
-    sing(number + "! ");
-    number = getNextPrime(number);
-    sing(number + "! ");
-    number = getNextPrime(number);
-    sing(number + "! ");
-    sing("These are the primes, that we find fine!");
-  }
-  private int getNextPrime(int number)
-  {
-    switch (number)
-    {
-      case 13 :
-        return 17;
-      case 17 :
-        return 19;
-      case 19 :
-        return 23;
-      case 23 :
-        return 29;
+import java.util.function.IntUnaryOperator;
+
+public class Lesson3HigherOrderFunctions extends Song {
+    public void singCheers() {
+        singSong(2, this::getNumber);
+        sing("Who do we appreciate?");
+        singSong(17, this::getNextPrime);
+        sing("These are the primes, that we find fine!");
     }
-    return 0;
-  }
+
+    private void singSong(int number, IntUnaryOperator nextNumber) {
+        for (int i = 0; i < 4; i++) {
+            sing(number + "! ");
+            number = nextNumber.applyAsInt(number);
+        }
+    }
+
+    private int getNumber(int number) {
+        return number + 2;
+    }
+
+    private int getNextPrime(int number) {
+        switch (number) {
+            case 13:
+                return 17;
+            case 17:
+                return 19;
+            case 19:
+                return 23;
+            case 23:
+                return 29;
+        }
+        return 0;
+    }
 }
